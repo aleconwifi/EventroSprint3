18468023fe917d17d5e1916e07be88f045b5499d
Alejandro Marcano <alejandro.marcano@correo.unimet.edu.ve>
2018-11-03 00:07:46 -0400

    pantalla de login

---
+++ src/pages/login/login.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  email: string;
+  password: string;
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+}
