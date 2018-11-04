cb88afd6d6501e8e28a1a2f918afe712257f01b6
Alejandro Marcano <alejandro.marcano@correo.unimet.edu.ve>
2018-11-04 00:08:34 -0400

    alert controller para mostrar validaciones PRUEBAS LISTAS REGISTRO

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, 
+        NavController, //navegacion entre paginas
+         NavParams, 
+         LoadingController, //mostrar animacion de cargando
+         AlertController } from 'ionic-angular'; //mostrar errores en registro
 import { RegisterProvider } from '../../providers/register/register';
 
 
@@ -14,10 +18,13 @@ export class RegisterPage {
   email: string;
   password: string;
 
+  loading: any;
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams, 
-              public reg: RegisterProvider) {
+              private reg: RegisterProvider,
+              private loadingCtrl: LoadingController,
+              private alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
@@ -29,11 +36,58 @@ export class RegisterPage {
 
   }
 
+
   userSignup(){
-    this.reg.registerUser(this.fullname, this.email, this.password)
-    .subscribe(res =>{
-      console.log(res);
-    });
+    //PRUEBAS VALIDACIONES AL DEJAR CAMPOS VACIOS
+    if(this.fullname !== undefined || this.email !== undefined || this.password !== undefined){
+    //subscribe de registro, llama al metodo del servicio
+
+      this.showLoading();
+      this.reg.registerUser(this.fullname, this.email, this.password)
+        .subscribe(res => {
+          this.loading.dismiss();
+          //si se crea el usuario, lo mando al home
+          if(res.user){
+           // this.storage.set('useremail', res.user.email);
+            this.navCtrl.setRoot("HomePage");
+          }
+
+          if(res.error){
+            //Alert Controller, desplegarlo si hay error y mostrar el error del backend
+            let alert = this.alertCtrl.create({
+              title: 'Error de registro',
+              subTitle: res.error,
+              buttons: ['OK']
+            });
+
+            alert.present();
+          }
+        });
+
+        this.fullname = '';
+        this.password = '';
+        this.email = '';
+    } else {
+      let alert = this.alertCtrl.create({
+        title: 'Error de Registro',
+        subTitle: 'No puedes colocar campos vacios',
+        buttons: ['OK']
+      });
+
+      alert.present();
+    }
   }
+  
+  //ventana que aparece mientras carga el registro
+  showLoading(){
+    this.loading = this.loadingCtrl.create({
+      content: 'Cargando...no pises el saman',
+      duration: 3000
+    });
+
+    this.loading.present(); //monstar el loading
+  }
+
+
 
 }
--- src/providers/register/register.ts
+++ src/providers/register/register.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from '../../../node_modules/rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 
 
 @Injectable()
